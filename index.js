252311b9ee675674f9eaab26c3842323dea46ed3
AskatAsh <abdulaskat@gmail.com>
2024-12-14 14:26:03 +0600

    changed database name and password in .env file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ console.log(process.env.USER_DB);
 // mongoDB database connection code
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.fisbs9h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fisbs9h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
