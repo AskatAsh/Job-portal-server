9862073913dc40c47a259d132918396d118ea846
AskatAsh <abdulaskat@gmail.com>
2024-12-20 19:21:06 +0600

    used req.query to get email and get jobs with email from database in jobs route

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ async function run() {
 
     // get all jobs data
     app.get("/jobs", async (req, res) => {
-      const cursor = jobsCollection.find();
+      const email = req.query.email;
+      let query = {};
+      if(email){
+        query = {hr_email: email};
+      }
+      const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
